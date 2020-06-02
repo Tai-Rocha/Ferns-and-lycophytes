a619704fbb3a156c7416b0f91d569e08b18437ec
Tai-Rocha <taina013@gmail.com>
2020-06-02 20:46:03 -0300

    PCA_INPE

--- scripts/PCA_INPE.R
+++ scripts/PCA_INPE.R
@@ -19,51 +19,51 @@ library(vegan)
 
 ## Read and Load data
 
-inpe_vals_pca <- read.csv("./data/PCA/WC_VALS_Original.csv", sep = ",", dec = ".")
+inpe_vals_pca <- read.csv("./data/PCA/INPE_VALS.csv", sep = ",", dec = ".")
 
 ### standardize unit of variables to build PCA  
 
-wc_vals<-decostand(wc_vals_pca[3:21], method="standardize", scla)
+#wc_vals<-decostand(wc_vals_pca[3:6], method="standardize", scla)
 
-write.table(wc_vals, './data/wc_vals_standardize.txt')
+#write.table(wc_vals, './data/wc_vals_standardize.txt')
 
 ### PCA
 
-wc.res.pca <- prcomp(wc_vals_pca[3:21],  scale = TRUE)
+inpe.res.pca <- prcomp(inpe_vals_pca[3:6],  scale = TRUE)
 
 
 
 # Color individuals by groups
-fviz_pca_ind(wc.res.pca, label="none", habillage=wc_vals_pca$Setor)
+fviz_pca_ind(inpe.res.pca, label="none", habillage=inpe_vals_pca$Setor)
 
 
-p <- fviz_pca_ind(wc.res.pca, label="none", habillage=wc_vals_pca$Setor,
+p <- fviz_pca_ind(inpe.res.pca, label="none", habillage=inpe_vals_pca$Setor,
                   addEllipses=TRUE, ellipse.level=0.95)
 print(p)
 
 # Change the color by groups, add ellipses
-fviz_pca_biplot(wc.res.pca, label="var", habillage=wc_vals_pca$Setor,
+fviz_pca_biplot(inpe.res.pca, label="var", habillage=inpe_vals_pca$Setor,
                 addEllipses=TRUE, ellipse.level=0.95)
 
 
 
 
 #Select the top 3 contributing variables
-fviz_pca_var(wc.res.pca, select.var = list(contrib = 5))
+fviz_pca_var(inpe.res.pca, select.var = list(contrib = 2))
 
 
 # PCA with cor matriz 
 
-PCA_WC <- PCA(wc_vals_pca[3:21], scale.unit = T, ncp = 5, graph = TRUE, ind.sup = 20:21)
+PCA_INPE <- PCA(inpe_vals_pca[3:6], scale.unit = T, ncp = 7, graph = TRUE)
 
 
-plot(PCA_WC, graph.type = "ggplot")
+plot(PCA_INPE, graph.type = "ggplot")
 
-plot(PCA_WC, habillage = 10, cex=0.3)
+plot(PCA_INPE, habillage = 10, cex=0.3)
 
 
 
-gr <- plot(PCA_WC)
+gr <- plot(PCA_INPE)
 gr + theme(panel.grid.major = element_blank(),
            plot.title=element_text(size=14, color="blue"),
            axis.title = element_text(size=12, color="red"))
